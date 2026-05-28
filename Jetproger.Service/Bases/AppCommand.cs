134b28bf46c8a2ece7338256afe24d2a3e23632d
Vadim Kovalev <KovalevVaV@digtp.com>
2026-05-28 20:40:15 +0300

    Файлы конфигурации

---
+++ Jetproger.Service/Bases/AppCommand.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+using NLog;
+
+namespace Jetproger.Service.Bases
+{
+    public class AppCommand : MarshalByRefObject
+    {
+        public void Reset() { _buffer = new byte[4096]; }
+        public Stream ResponseReader { get; private set; }
+        public Stream ResponseWriter { get; private set; }
+        public Stream RequestReader { get; private set; }
+        public Stream RequestWriter { get; private set; }
+        public Exception Error { get; private set; }
+        private Stream _currentReader;
+        private Stream _currentWriter;
+        private byte[] _buffer;
+
+        public AppCommand(HttpListenerContext context)
+        {
+            ResponseWriter = context.Response.OutputStream;
+            RequestReader = context.Request.InputStream;
+            RequestWriter = new MemoryStream();
+            Reset();
+        }
+
+        public void Execute()
+        {
+            _currentReader = RequestReader;
+            _currentWriter = RequestWriter;
+            _currentReader.BeginRead(_buffer, 0, _buffer.Length, TryEndRead, this);
+        }
+
+        private void EndRead(IAsyncResult ar, AppCommand state)
+        {
+            var read = state._currentReader.EndRead(ar);
+            if (read > 0)
+            {
+                state._currentWriter.BeginWrite(state._buffer, 0, read, TryEndWrite, state);
+                return;
+            }
+            if (ReferenceEquals(_currentReader, ResponseReader))
+            {
+                state.Dispose();
+                return;
+            }
+            //var request = MemToStr((MemoryStream)state.StreamWriter);
+            //var response = AppProcess.HttpExecute(request); 
+            _currentReader = ResponseReader;
+            _currentWriter = ResponseWriter;
+            _currentReader.BeginRead(_buffer, 0, _buffer.Length, TryEndRead, this);
+        }
+
+        private void EndWrite(IAsyncResult ar, AppCommand state)
+        {
+            state._currentWriter.EndWrite(ar);
+            state.Reset();
+            state._currentReader.BeginRead(state._buffer, 0, state._buffer.Length, TryEndRead, state);
+        }
+
+        private void TryEndRead(IAsyncResult ar)
+        {
+            var state = (AppCommand)null;
+            try
+            {
+                state = (AppCommand)ar.AsyncState;
+                EndRead(ar, state);
+            }
+            catch (Exception e)
+            {
+                if (state != null) state.Dispose();
+                f.log.Error(e);
+            }
+        }
+
+        private void TryEndWrite(IAsyncResult ar)
+        {
+            var state = (AppCommand)null;
+            try
+            {
+                state = (AppCommand)ar.AsyncState;
+                EndWrite(ar, state);
+            }
+            catch (Exception e)
+            {
+                if (state != null) state.Dispose();
+                f.log.Error(e);
+            }
+        }
+
+        #region dispose
+
+        public void Dispose()
+        {
+            DisposeResponseWriter();
+            DisposeResponseReader();
+            DisposeRequestWriter();
+            DisposeRequestReader();
+        }
+
+        public void DisposeResponseWriter()
+        {
+            try
+            {
+                if (ResponseWriter == null) return;
+                ResponseWriter.Dispose();
+                ResponseWriter = null;
+            }
+            catch (Exception e)
+            {
+                ResponseWriter = null;
+                Error = e;
+            }
+        }
+
+        public void DisposeResponseReader()
+        {
+            try
+            {
+                if (ResponseReader == null) return;
+                ResponseReader.Dispose();
+                ResponseReader = null;
+            }
+            catch (Exception e)
+            {
+                ResponseReader = null;
+                Error = e;
+            }
+        }
+
+        public void DisposeRequestWriter()
+        {
+            try
+            {
+                if (RequestWriter == null) return;
+                RequestWriter.Dispose();
+                RequestWriter = null;
+            }
+            catch (Exception e)
+            {
+                RequestWriter = null;
+                Error = e;
+            }
+        }
+
+        public void DisposeRequestReader()
+        {
+            try
+            {
+                if (RequestReader == null) return;
+                RequestReader.Dispose();
+                RequestReader = null;
+            }
+            catch (Exception e)
+            {
+                RequestReader = null;
+                Error = e;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Jetproger.Service/Bases/AppProcess.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Jetproger.Service.Bases
+{
+    public static class AppProcess
+    {
+        private static MethodInfo MainMethod => GetOne(_mainHolder, () => Provider.GetType().GetMethod("MainExecute", BindingFlags.Instance | BindingFlags.Public));
+        private static readonly MethodInfo[] _mainHolder = { null };
+
+        private static MethodInfo BaseMethod => GetOne(_baseHolder, () => Provider.GetType().GetMethod("BaseExecute", BindingFlags.Instance | BindingFlags.Public));
+        private static readonly MethodInfo[] _baseHolder = { null };
+
+        private static MethodInfo HttpMethod => GetOne(_httpHolder, () => Provider.GetType().GetMethod("HttpExecute", BindingFlags.Instance | BindingFlags.Public));
+        private static readonly MethodInfo[] _httpHolder = { null };
+
+        private static MethodInfo PingMethod => GetOne(_pingHolder, () => Provider.GetType().GetMethod("PingExecute", BindingFlags.Instance | BindingFlags.Public));
+        private static readonly MethodInfo[] _pingHolder = { null };
+
+        private static object Provider => GetOne(_providerHolder, CreateProvider);
+        private static readonly object[] _providerHolder = { null };
+
+        public static void StartCommands() { _stop = false; }
+        public static void StopCommands() { _stop = true; }
+        private static AppDomain _domain;
+        private static bool _stop;
+
+        public static bool MainExecute(string[] args)
+        {
+            if (_stop) return false;
+            try
+            {
+                return (bool)MainMethod.Invoke(Provider, new object[] { f.exe.name, args });
+            }
+            catch (Exception e)
+            {
+                ErrorProcessing(e);
+                return true;
+            }
+        }
+
+        public static void BaseExecute()
+        {
+            if (_stop) return;
+            try
+            {
+                BaseMethod.Invoke(Provider, new object[] { f.exe.name });
+            }
+            catch (Exception e)
+            {
+                ErrorProcessing(e);
+            }
+        }
+
+        public static string HttpExecute(string request)
+        {
+            if (_stop) return null;
+            try
+            {
+                return (string)HttpMethod.Invoke(Provider, new object[] { request });
+            }
+            catch (Exception e)
+            {
+                ErrorProcessing(e);
+                return string.Empty;
+            }
+        }
+
+        private static bool PingExecute()
+        {
+            try
+            {
+                return (bool)PingMethod.Invoke(Provider, new object[0]);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void Reset()
+        {
+            lock (_providerHolder)
+            {
+                TryUnloadDomain();
+                _providerHolder[0] = null;
+                _baseHolder[0] = null;
+                _httpHolder[0] = null;
+                _pingHolder[0] = null;
+                _mainHolder[0] = null;
+            }
+        }
+
+        private static void TryUnloadDomain()
+        {
+            try
+            {
+                if (_domain != null) AppDomain.Unload(_domain);
+                _domain = null;
+            }
+            catch
+            {
+                _domain = null;
+            }
+        }
+
+        private static void ErrorProcessing(Exception e)
+        {
+            f.log.Error(e);
+            if (!PingExecute()) Reset();
+        }
+
+        private static object CreateProvider()
+        {
+            var setup = new AppDomainSetup
+            {
+                ConfigurationFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile,
+                ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                LoaderOptimization = LoaderOptimization.SingleDomain,
+                ShadowCopyFiles = "true",
+            };
+            var name = $"APPDOMAIN{Guid.NewGuid().ToString().Replace("-", "")}".ToUpper();
+            _domain = AppDomain.CreateDomain(name, AppDomain.CurrentDomain.Evidence, setup);
+            return _domain.CreateInstanceFromAndUnwrap(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "", "Jetproger.Tools.Convert.dll"), "Jetproger.Tools.Convert.Commanders.CommandProvider");
+        }
+
+        private static T GetOne<T>(T[] holder, Func<T> factory) where T : class
+        {
+            if (holder[0] == null)
+            {
+                lock (holder)
+                {
+                    if (holder[0] == null) holder[0] = factory();
+                }
+            }
+            return holder[0];
+        }
+    }
+}
